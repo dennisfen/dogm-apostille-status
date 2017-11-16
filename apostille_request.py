5500e8b77ccf485062bec713467c4a72e8d21ce7
Denis Borisevich <denis.borisevich@me.com>
2017-11-16 22:55:19 +0300

    Moved all request functionality inside class

--- apostille_request.py
+++ apostille_request.py
@@ -4,8 +4,6 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-dogm_url = 'https://dogm-1-trp.mos.ru/Search'
-
 def read_entries(filename):
     try:
         with open(filename, 'r') as f:
@@ -18,22 +16,39 @@ def read_entries(filename):
             
     return None
 
-def main():
-    entries = read_entries('entries.json')
-    for entry in entries['entry']:
-        payload = {
+class ApostilleChecker():
+    dogm_url = 'https://dogm-1-trp.mos.ru/Search'
+
+    def __init__(self, entry):
+        self.payload = {
                 'RegNumber': entry['reg_num'],
                 'HolderLastName': entry['last_name'],
                 'HolderFirstName': entry['first_name']
         }
-        r = requests.get(dogm_url, data=payload)
+        self.status = None
+
+    def __str__(self):
+        return "{0}: {1}".format(self.payload['RegNumber'], self.status)
+
+
+    def update_status(self):
+        r = requests.get(self.dogm_url, data=self.payload)
         page = r.text
         soup = BeautifulSoup(page, 'html.parser')
+        
         # there are no ids or anything useful to track down needed info
         # so we have to search for the only <h4></h4> header tag present
         # on that page
-        status = soup.find('h4')
-        print(entry['reg_num'] + ': ' + status.string)
+        self.status = soup.find('h4').string
+        return self.status
+
+
+def main():
+    entries = read_entries('entries.json')
+    for entry in entries['entry']:
+        checker = ApostilleChecker(entry)
+        checker.update_status()
+        print(checker)
 
 
 if __name__ == '__main__':
