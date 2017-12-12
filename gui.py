3c0449acc4c346a5c41e4d249c7b34b1785492ef
Denis Borisevich <denis.borisevich@me.com>
2017-12-13 02:27:58 +0300

    Some semi-working GUI

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from time import sleep
 
 from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import (QApplication, QWidget, QTableView, QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QTableView, QVBoxLayout, QPushButton)
 
 from apostille_request import ApostilleChecker, read_entries
 
@@ -21,8 +21,7 @@ class RequestThread(QThread):
 
     def run(self):
         self.checker.request_status()
-        sleep(5)
-        print(self.checker)
+        #print(self.checker)
 
 
 class MainWindow(QWidget):
@@ -52,7 +51,8 @@ class MainWindow(QWidget):
 
         for idx, apostille in enumerate(self.apostilles):
             self.apostille_threads.append(RequestThread(apostille))
-            self.apostille_threads[idx].start()
+            self.apostille_threads[idx].finished.connect(self.got_update)
+            #self.apostille_threads[idx].start()
 
         self.table_model.setHorizontalHeaderLabels(["Номер", "Имя", "Статус"])
         self.table_view.setModel(self.table_model)
@@ -60,10 +60,30 @@ class MainWindow(QWidget):
         self.table_view.resizeColumnsToContents()
         self.v_box.addWidget(self.table_view)
 
+        self.btn_threads = btn_threads = QPushButton("Fire threads")
+        btn_threads.clicked.connect(self.start_threads)
+
+        self.v_box.addWidget(btn_threads)
+
         self.setLayout(self.v_box)
         self.setMinimumSize(500, 200)
         self.setWindowTitle('Статус подготовки апостилей')
         self.show()
+
+    def start_threads(self):
+        print("threads fired")
+        for t in self.apostille_threads:
+            t.start()
+
+    def got_update(self):
+        sender = self.sender()
+        print("here\'s update: {0} = {1}".format(
+            self.apostille_threads.index(sender),
+            sender))
+
+        row = self.apostille_threads.index(sender)
+        item = self.table_model.item(row, 2)
+        item.setText(self.apostilles[row].status_string) #connect them, maybe?
 
 
 if __name__ == '__main__':
